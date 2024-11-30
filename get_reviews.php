2393554a1c1a5efc0c74b68941467ab7bc34e7b9
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2024-11-30 16:23:51 +0900

    Fix: Keep review dates on mobile being displayed as 'NaNy'

--- get_reviews.php
+++ get_reviews.php
@@ -2,7 +2,7 @@
 
 include('db_connect.php');
 
-// Get hodtel ID via query parameter:
+// Get hostel ID via query parameter:
 $hostel_id = isset($_GET['id']) ? intval($_GET['id']) : 0; 
 
 // Get reviewLimit / reviewOffset via query parameter:
@@ -10,10 +10,17 @@ $reviewLimit = isset($_GET['reviewLimit']) ? intval($_GET['reviewLimit']) : 10;
 $reviewOffset = isset($_GET['reviewOffset']) ? intval($_GET['reviewOffset']) : 0;
 
 try {
-    $stmt = $conn -> prepare('SELECT * FROM reviews WHERE hostel_id = :hostel_id LIMIT ' . $reviewLimit . ' OFFSET ' . $reviewOffset); 
-    $stmt -> bindParam(':hostel_id', $hostel_id, PDO::PARAM_INT);
-    $stmt -> execute();
-    $reviews = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+    $stmt = $conn->prepare('SELECT * FROM reviews WHERE hostel_id = :hostel_id LIMIT ' . $reviewLimit . ' OFFSET ' . $reviewOffset); 
+    $stmt->bindParam(':hostel_id', $hostel_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $reviews = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Format the date to ISO 8601.
+    // This is an attempt to fix the mobile-displaying-'NaNy'-as-dates problem. Not yet sure if it works.
+    foreach ($reviews as &$review) {
+        $date = new DateTime($review['date_posted']);
+        $review['date_posted'] = $date->format(DateTime::ATOM); // ISO 8601 format
+    }
 
     header("Content-Type: application/json");
     echo json_encode($reviews);
