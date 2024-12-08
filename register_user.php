378c30b908e2b6ab44a6c92e63c7d6e9ab246c0b
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2024-12-08 23:15:59 +0900

    Add: register_user.php - Takes usernames/emails and inserts in 'users' table

--- register_user.php
+++ register_user.php
@@ -1,5 +1,49 @@
 <?php
 
+include('db_connect.php');
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = htmlspecialchars($_POST['username']);
+    $email = htmlspecialchars($_POST['email']);
+
+    // Throw error is email is invalid:
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { 
+        echo json_encode(['status' => 'error', 'message' => 'Invalid email format']);
+        exit;
+    }
+
+    // Check if username already exists:
+    $stmt = $conn -> prepare("SELECT * FROM users WHERE username = :username");
+    $stmt -> bindParam(':username', $username, PDO::PARAM_STR);
+    $stmt -> execute();
+
+    if ($stmt -> rowCount() > 0) {
+        echo json_encode(['status' => 'error', 'message' => 'Username already taken.']);
+        exit;
+    }
+
+    // Check if email already exists:
+    $stmt = $conn -> prepare("SELECT * FROM users WHERE email = :email");
+    $stmt -> bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt -> execute();
+
+    if ($stmt -> rowCount() > 0) {
+        echo json_encode(['status' => 'error', 'message' => 'Email already registered.']);
+        exit;
+    }
+
+    // Insert new user into db:
+    $stmt = $conn -> prepare("INSERT INTO users (username, email) VALUES (:username, :email)");
+    $stmt -> bindParam(':username', $username, PDO::PARAM_STR);
+    $stmt -> bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt -> execute();
+
+    // Return insert statement status:
+    if ($stmt->execute()) {
+        echo json_encode(['status' => 'success', 'message' => 'Registration successful.']);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Registration failed.']);
+    }
+}
 
 ?>
